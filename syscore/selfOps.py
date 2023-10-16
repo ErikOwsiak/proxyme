30b3d3353c28a984bef3891aca128714826b5dfd
ErikOwsiak <erik@iotech.systems>
2023-10-17 01:31:02 +0200

    self.db work

--- syscore/selfOps.py
+++ syscore/selfOps.py
@@ -1,18 +1,45 @@
 
-import sqlite3
+import typing as t
+import os.path, sqlite3
+import uuid
+# -- system --
+from syscore.systypes import *
 
 
-class selfOps(sqlite3):
+HOME_PATH: str = "/home/erik"
+DATA_PATH: str = f"{HOME_PATH}/proxyme/_self/data"
 
-   NAME: str = "self"
+
+class selfOps(object):
 
    def __init__(self):
-      pass
+      self.path: str = f"{DATA_PATH}/self.db"
+      self.conn: sqlite3.Connection = t.Any
 
-   def imprint(self, data: str) -> str:
+   def init(self):
+      if not os.path.exists(self.path):
+         cwd: str = os.getcwd()
+         raise FileNotFoundError(self.path)
+      # -- -- -- --
+      self.conn: sqlite3.Connection = sqlite3.connect(self.path)
+
+   def imprint(self, _type: str, data: str) -> [sysCode, None | str]:
       try:
-         path: str = f"_self/data/{selfOps.NAME}.db"
-         conn = self.connect(path)
-         return data
+         guid = uuid.uuid4().hex
+         qry: str = (f"insert into patterns_in values('{guid}',"
+            f" datetime('now'), '{_type}', '{data}');")
+         cur: sqlite3.Cursor = self.conn.cursor()
+         res = cur.execute(qry)
+         if res.rowcount != 1:
+            pass
+         # -- -- -- --
+         self.conn.commit()
+         # -- -- -- --
+         return sysCode.OK, None
       except Exception as e:
-         print(e)
+         return sysCode.ERROR, str(e)
+      finally:
+         pass
+
+   def process(self):
+      pass
--- syscore/systypes.py
+++ syscore/systypes.py
@@ -6,3 +6,8 @@ class httpMethod(enum.IntEnum):
    POST = 0
    GET = 1
    PUT = 2
+
+
+class sysCode(enum.IntEnum):
+   OK = 0
+   ERROR = 1000
